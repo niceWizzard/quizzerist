cf73d308576a1d1ead641a15f0cd49efbdebf13b
niceWizzard <richard232manansala@gmail.com>
2025-09-22 17:06:06 +0800

    Installed compose navigation library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    
 }
 
 android {
@@ -59,5 +61,7 @@ dependencies {
 
     implementation(libs.koin.androidx.compose)
     implementation(libs.koin.android)
+    implementation(libs.androidx.navigation.compose)
+
 
 }
